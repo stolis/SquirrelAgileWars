6ab7221a7fd3997fbab0b817797a36a98d9b67e2
stolis <akokkalakis@gmail.com>
2018-06-06 19:11:49 +0300

    Initial commit
    Initial commit

---
+++ public/js/game.js
@@ -0,0 +1,30 @@
+var config = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: { y: 0 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  } 
+};
+ 
+var game = new Phaser.Game(config);
+ 
+function preload() {}
+ 
+function create() {
+    
+    //init and assign network module
+    this.socket = io();
+}
+ 
+function update() {}
