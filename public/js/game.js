a202449a8564f646e0edb548dd481b6ecf3f4180
stolis <akokkalakis@gmail.com>
2018-06-06 20:30:23 +0300

    added player functions

--- public/js/game.js
+++ public/js/game.js
@@ -19,12 +19,63 @@ var config = {
  
 var game = new Phaser.Game(config);
  
-function preload() {}
+function preload() {
+    this.load.image('ship', 'assets/spaceShips_001.png');
+}
  
 function create() {
+    var self = this;
     
     //init and assign network module
     this.socket = io();
+    this.otherPlayers = this.physics.add.group();
+
+    //event fire of other players joining
+    this.socket.on('currentPlayers', function (players){
+        Object.keys(players).forEach(function (id){
+            if (players[id].playerId === self.socket.id){
+                addPlayer(self, players[id]);
+            } else {
+                addOtherPlayers(self, players[id]);
+            }
+        });
+    });
+
+    //event fire of this player joining
+    this.socket.on('newPlayer', function (playerInfo){
+        addOtherPlayers(self, playerInfo);
+    });
+
+    this.socket.on('disconnnect', function (playerId){
+        self.otherPlayers.getChildren().forEach(function (otherPlayer){
+            if (playerId === otherPlayer.playerId){
+                otherPlayer.destroy();
+            }
+        });
+    });
 }
  
 function update() {}
+
+function addPlayer(self, playerInfo) {
+    self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    if (playerInfo.team === 'blue') {
+      self.ship.setTint(0x0000ff);
+    } else {
+      self.ship.setTint(0xff0000);
+    }
+    self.ship.setDrag(100);
+    self.ship.setAngularDrag(100);
+    self.ship.setMaxVelocity(200);
+}
+
+function addOtherPlayers(self, playerInfo) {
+    const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    if (playerInfo.team === 'blue') {
+      otherPlayer.setTint(0x0000ff);
+    } else {
+      otherPlayer.setTint(0xff0000);
+    }
+    otherPlayer.playerId = playerInfo.playerId;
+    self.otherPlayers.add(otherPlayer);
+}
--- server.js
+++ server.js
@@ -29,6 +29,10 @@ io.on('connection', function (socket){
 
   socket.on('disconnect', function (){
     console.log('user disconnected');
+    // remove this player from our players object
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    io.emit('disconnect', socket.id);
   });
 });
 
