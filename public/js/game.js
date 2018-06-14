147128f1870399fb44af3241da87d1e158574e87
stolis <akokkalakis@gmail.com>
2018-06-14 18:43:31 +0300

    Brute atlas instead

--- public/js/game.js
+++ public/js/game.js
@@ -3,6 +3,7 @@ var config = {
   parent: 'phaser-example',
   width: 800,
   height: 600,
+  pixelArt: true,
   physics: {
     default: 'arcade',
     arcade: {
@@ -16,12 +17,16 @@ var config = {
     update: update
   } 
 };
- 
+
+var player;
+var playerWalkLeft;
+var otherPlayers;
 var game = new Phaser.Game(config);
 
 function preload() {
     this.load.image('ship', './assets/spaceShips_001.png');
-    this.load.image('otherPlayer', './assets/enemyBlack5.png')
+    this.load.image('otherPlayer', './assets/enemyBlack5.png');
+    this.load.atlas('brute_001', './assets/Brute-001-atlas.png', '../assets/Brute-001.json');
 }
  
 function create() {
@@ -69,38 +74,46 @@ function create() {
         });
       });
 
+    //Create animation of spritesheet
     this.cursors = this.input.keyboard.createCursorKeys();
+
 }
  
 function update() {
-    if (this.ship) {
+    if (this.player) {
         if (this.cursors.left.isDown) {
-          this.ship.setAngularVelocity(-150);
+          this.player.setVelocityX(-150);
+          this.player.anims.play('walkleft', true);
+          //this.playerWalkLeft.play(30, true);
         } else if (this.cursors.right.isDown) {
-          this.ship.setAngularVelocity(150);
+          this.player.setVelocityX(150);
+        } else if (this.cursors.up.isDown){
+          this.player.setVelocityY(-150);
+        } else if (this.cursors.down.isDown) {
+          this.player.setVelocityY(150);
         } else {
-          this.ship.setAngularVelocity(0);
+            this.player.setVelocity(0);
         }
       
-        if (this.cursors.up.isDown) {
+        /*if (this.cursors.up.isDown) {
           this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
         } else {
           this.ship.setAcceleration(0);
-        }
+        }*/
 
         // emit player movement
-        var x = this.ship.x;
-        var y = this.ship.y;
-        var r = this.ship.rotation;
-        if (this.ship.oldPosition && (x !== this.ship.oldPosition.x || y !== this.ship.oldPosition.y || r !== this.ship.oldPosition.rotation)) {
-            this.socket.emit('playerMovement', { x: this.ship.x, y: this.ship.y, rotation: this.ship.rotation });
+        var x = this.player.x;
+        var y = this.player.y;
+        var r = this.player.rotation;
+        if (this.player.oldPosition && (x !== this.player.oldPosition.x || y !== this.player.oldPosition.y || r !== this.player.oldPosition.rotation)) {
+            this.socket.emit('playerMovement', { x: this.player.x, y: this.player.y, rotation: this.player.rotation });
         }
         
         // save old position data
-        this.ship.oldPosition = {
-            x: this.ship.x,
-            y: this.ship.y,
-            rotation: this.ship.rotation
+        this.player.oldPosition = {
+            x: this.player.x,
+            y: this.player.y,
+            rotation: this.player.rotation
         };
       
         //this.physics.world.wrap(this.ship, 5);
@@ -108,15 +121,28 @@ function update() {
 }
 
 function addPlayer(self, playerInfo) {
-    self.ship = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
-    if (playerInfo.team === 'blue') {
+    //self.ship.setCollideWorldBounds(true);
+    self.player = self.physics.add.sprite(200, 200, 'brute_001');
+    //self.playerWalkLeft = self.player.anims.add('walkLeft', Phaser.Animation.generateFrameNames('brute_001 ', 48, 55, '.ase', 1));
+    var sdf = self.anims.generateFrameNames('brute_001', { prefix: 'Brute_001 ', start: 48, end: 55, suffix: '.aseprite', zeroPad: 1 });
+    console.log(sdf);
+    self.anims.create({
+        key: 'walkleft',
+        frames: self.anims.generateFrameNames('brute_001', { prefix: 'Brute_001 ', start: 48, end: 55, suffix: '.aseprite', zeroPad: 1 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    self.player.setCollideWorldBounds(true);
+    //self.ship = self.physics.add.sprite(playerInfo.x, playerInfo.y, 'brute_001').setOrigin(0.5, 0.5).setDisplaySize(32, 32);
+    //console.log(self.ship);
+    /*if (playerInfo.team === 'blue') {
       self.ship.setTint(0x0000ff);
     } else {
       self.ship.setTint(0xff0000);
-    }
-    self.ship.setDrag(100);
+    }*/
+    /*self.ship.setDrag(100);
     self.ship.setAngularDrag(100);
-    self.ship.setMaxVelocity(200);
+    self.ship.setMaxVelocity(200);*/
 }
 
 function addOtherPlayers(self, playerInfo) {
